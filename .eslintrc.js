d6e2657382910c17934e421cc4a61a2068f7ce27
Nikhil-Kumaran <nikhilkumaran.dev@gmail.com>
2021-03-01 20:07:25 +0530

    test commit to check if precommit hook works

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,9 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
-    // ecmaFeatures: {
-    //   jsx: true, // Allows for the parsing of JSX
-    // },
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
   },
   settings: {
     react: {
@@ -20,8 +20,8 @@ module.exports = {
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "react/jsx-uses-react": "off",
-    "react/react-in-jsx-scope": "off"
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
   },
 };
