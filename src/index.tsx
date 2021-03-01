a2141f80b3cc268b968995cf1188d705ec5cb8ac
Nikhil-Kumaran <nikhilkumaran.dev@gmail.com>
2021-03-01 19:14:02 +0530

    test commit to check if precommit hook works

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from "react";
 import styles from './styles.module.css';
 
 interface Props {
@@ -6,5 +6,5 @@ interface Props {
 }
 
 export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component - {text}</div>;
+  return <div className={styles.test}>Example Component - {text}</div>
 };
