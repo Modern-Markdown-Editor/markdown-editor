f5b5413c2f8fcb222d1fdfbe028e901df9ec4e21
Nikhil-Kumaran <nikhilkumaran.dev@gmail.com>
2021-03-27 16:50:15 +0530

    add editable block

---
+++ src/components/EditableBlock.tsx
@@ -0,0 +1,158 @@
+import * as React from 'react';
+import { DropdownItem } from './Dropdown';
+import styles from '../styles.module.css';
+import uuid from '../utils/uuid';
+
+interface EditableBlockProps {
+  triggerKey: string;
+  dropdownisOpen: boolean;
+  setDropdownIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setActiveItemIndex: React.Dispatch<React.SetStateAction<number>>;
+  activeItemIndex: number;
+  items: {
+    name: string;
+    description: string;
+    tag: string;
+  }[];
+  setSelectedItem: React.Dispatch<React.SetStateAction<DropdownItem | undefined>>;
+  placeholder: string;
+  setRangeValues: React.Dispatch<
+    React.SetStateAction<{
+      top: number;
+      left: number;
+    }>
+  >;
+  block: {
+    uuid: string;
+    tag: string;
+    content: string;
+  };
+  blocks: {
+    uuid: string;
+    tag: string;
+    content: string;
+  }[];
+  setBlocks: React.Dispatch<
+    React.SetStateAction<
+      {
+        uuid: string;
+        tag: string;
+        content: string;
+      }[]
+    >
+  >;
+  setBlockFocus: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export const EditableBlock = ({
+  triggerKey,
+  dropdownisOpen,
+  setRangeValues,
+  setDropdownIsOpen,
+  setActiveItemIndex,
+  activeItemIndex,
+  items,
+  setSelectedItem,
+  placeholder,
+  block,
+  blocks,
+  setBlocks,
+  setBlockFocus,
+}: EditableBlockProps) => {
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (dropdownisOpen) {
+      event.preventDefault();
+    }
+
+    switch (event.key) {
+      case triggerKey: {
+        let range = document.createRange();
+        let offset = window.getSelection()?.anchorOffset as number;
+        let element = document.activeElement as Node;
+        range.setStart(element.firstChild as Node, offset);
+        range.setEnd(element.firstChild as Node, offset);
+
+        let rangeValue = range.getBoundingClientRect();
+        setRangeValues({ top: rangeValue.top, left: rangeValue.left });
+        setDropdownIsOpen(true);
+        break;
+      }
+      case 'ArrowUp': {
+        if (activeItemIndex === 0) {
+          return;
+        }
+        setActiveItemIndex(activeItemIndex - 1);
+        break;
+      }
+      case 'ArrowDown': {
+        if (activeItemIndex === items.length - 1) {
+          return;
+        }
+        setActiveItemIndex(activeItemIndex + 1);
+        break;
+      }
+      case 'Enter': {
+        if (!dropdownisOpen) {
+          event.preventDefault();
+          const tempBlocks = [...blocks];
+          const tempBlock = {
+            uuid: uuid(),
+            tag: 'p',
+            content: '',
+          };
+          tempBlocks.splice(tempBlocks.findIndex(({ uuid }) => uuid === block.uuid) + 1, 0, tempBlock);
+          setBlocks([...tempBlocks]);
+          setBlockFocus(tempBlock.uuid);
+        } else {
+          const tempBlocks = [...blocks];
+          const tempBlock = tempBlocks[tempBlocks.findIndex(({ uuid }) => uuid === block.uuid)];
+          tempBlock.tag = items[activeItemIndex].tag;
+          tempBlocks.splice(
+            tempBlocks.findIndex(({ uuid }) => uuid === block.uuid),
+            1,
+            tempBlock,
+          );
+          setBlocks([...tempBlocks]);
+          setDropdownIsOpen(false);
+        }
+
+        break;
+      }
+      default: {
+        if (dropdownisOpen) {
+          setDropdownIsOpen(false);
+        }
+        break;
+      }
+    }
+  };
+
+  console.log('block', block);
+
+  return React.createElement(block.tag || 'div', {
+    onKeyDown: handleKeyDown,
+    placeholder: placeholder || '',
+    className: `${styles.test}`,
+    contentEditable: true,
+    id: block.uuid,
+    onInput: (event: React.ChangeEvent<HTMLElement>) => {
+      console.log('event', event.target.innerText);
+
+      const tempBlocks = [...blocks];
+      const tempBlock = tempBlocks[tempBlocks.findIndex(({ uuid }) => uuid === block.uuid)];
+      // Change the content as we type.
+      tempBlock.content = event.target.innerText;
+      tempBlocks.splice(
+        tempBlocks.findIndex(({ uuid }) => uuid === block.uuid),
+        1,
+        tempBlock,
+      );
+      setBlocks([...tempBlocks]);
+
+      // Need to move the cursor to the end of the line
+    },
+    dangerouslySetInnerHTML: {
+      __html: block.content,
+    },
+  });
+};
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import styles from './styles.module.css';
 import './styles/base.css';
 import { Dropdown, DropdownItem } from './components/Dropdown';
+import { EditableBlock } from './components/EditableBlock';
+import uuid from './utils/uuid';
 export interface MarkdownProps {
   placeholder?: string;
   triggerKey?: string;
@@ -11,34 +13,45 @@ const items = [
   {
     name: 'Text',
     description: 'Plain text',
+    tag: 'p',
   },
   {
     name: 'Heading 1',
     description: 'Big section heading',
+    tag: 'h1',
   },
   {
     name: 'Heading 2',
     description: 'Medium section heading',
+    tag: 'h2',
   },
   {
     name: 'Heading 3',
     description: 'Small section heading',
-  },
-  {
-    name: 'Bullet list',
-    description: 'List with bullet points',
-  },
-  {
-    name: 'Numbered',
-    description: 'List with numbering',
+    tag: 'h3',
   },
 ];
+
+const initalBlock = {
+  uuid: uuid(),
+  tag: 'p',
+  content: '',
+};
 
 export const Markdown = ({ placeholder = 'Type Something', triggerKey = '/' }: MarkdownProps) => {
   const [activeItemIndex, setActiveItemIndex] = React.useState(0);
   const [selectedItem, setSelectedItem] = React.useState<DropdownItem>();
   const [dropdownisOpen, setDropdownIsOpen] = React.useState(false);
   const [rangeValues, setRangeValues] = React.useState({ top: 0, left: 0 });
+  const [blocks, setBlocks] = React.useState([initalBlock]);
+  const [blockFocus, setBlockFocus] = React.useState(initalBlock.uuid);
+
+  React.useEffect(() => {
+    console.log('blockFocus', blockFocus);
+    const element = document.getElementById(blockFocus);
+    element?.focus();
+  }, [blockFocus]);
+
   React.useEffect(() => {
     if (dropdownisOpen) {
       let top = 0;
@@ -67,71 +80,29 @@ export const Markdown = ({ placeholder = 'Type Something', triggerKey = '/' }: M
     setDropdownIsOpen(false);
   };
 
-  const handleKeyDown = (event: React.KeyboardEvent) => {
-    if (dropdownisOpen) {
-      event.preventDefault();
-    }
-    if (event.key == triggerKey) {
-      event.preventDefault();
-      let range = document.createRange();
-      let offset = window.getSelection()?.anchorOffset as number;
-      let element = document.activeElement as Node;
-      range.setStart(element.firstChild as Node, offset);
-      range.setEnd(element.firstChild as Node, offset);
-
-      let rangeValue = range.getBoundingClientRect();
-      setRangeValues({ top: rangeValue.top, left: rangeValue.left });
-      setDropdownIsOpen(true);
-      switch (event.key) {
-        case 'ArrowUp': {
-          if (activeItemIndex === 0) {
-            return;
-          }
-          setActiveItemIndex(activeItemIndex - 1);
-          break;
-        }
-        case 'ArrowDown': {
-          if (activeItemIndex === items.length - 1) {
-            return;
-          }
-          setActiveItemIndex(activeItemIndex + 1);
-          break;
-        }
-        case 'Enter': {
-          setSelectedItem(items[activeItemIndex]);
-          setDropdownIsOpen(false);
-          break;
-        }
-        default: {
-          if (dropdownisOpen) {
-            setDropdownIsOpen(false);
-          }
-          break;
-        }
-      }
-    } else {
-      if (event.key == 'Enter') {
-        event.preventDefault();
-        let element = document.getElementById('content');
-        let newElement = document.createElement('div') as HTMLElement;
-        newElement.setAttribute('contenteditable', 'true');
-        newElement.setAttribute('placeholder', placeholder);
-        newElement.setAttribute('class', `${styles.test}`);
-        element?.appendChild(newElement);
-        newElement.focus();
-      }
-    }
-  };
-
   return (
     <div id="content" className={styles.markdown}>
-      <div
-        contentEditable={true}
-        className={`${styles.test}`}
-        placeholder={placeholder}
-        // onInput={handleChange}
-        onKeyDown={handleKeyDown}
-      />
+      {blocks?.map((block) => {
+        return (
+          <EditableBlock
+            key={block.uuid}
+            activeItemIndex={activeItemIndex}
+            dropdownisOpen={dropdownisOpen}
+            items={items}
+            placeholder={placeholder}
+            setActiveItemIndex={setActiveItemIndex}
+            setDropdownIsOpen={setDropdownIsOpen}
+            setRangeValues={setRangeValues}
+            setSelectedItem={setSelectedItem}
+            triggerKey={triggerKey}
+            block={block}
+            blocks={blocks}
+            setBlocks={setBlocks}
+            setBlockFocus={setBlockFocus}
+          />
+        );
+      })}
+
       <Dropdown
         isOpen={dropdownisOpen}
         items={items}
