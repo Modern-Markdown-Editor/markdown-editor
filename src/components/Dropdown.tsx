28deb2b5cdb50920161c49583f74383aaf803862
Nikhil Kumaran <nikhilkumaran.dev@gmail.com>
2021-03-28 19:08:00 +0530

    📦 MD-8 MD-16 Configure and view the list of options as a dropdown

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,24 +1,34 @@
 import * as React from 'react';
 import styles from './Dropdown.module.css';
+import classNames from 'classnames';
 
+export interface DropdownItem {
+  name: string;
+  description: string;
+}
 interface DropdownProps {
-  items: {
-    name: string;
-    description: string;
-  }[];
-  isOpen: boolean;
+  items: DropdownItem[];
+  activeItemIndex: number;
+  handleOnMouseEnter: (index: number) => void;
+  handleItemClick: (item: DropdownItem) => void;
 }
 
-export const Dropdown = ({ items, isOpen }: DropdownProps) => {
-  if (!isOpen) {
-    return null;
-  }
+export const Dropdown = ({ items, activeItemIndex, handleOnMouseEnter, handleItemClick }: DropdownProps) => {
   return (
     <ul
-      className={`${styles.dropdown} rounded relative shadow-md inline-block m-2 max-w-xs min-w-md max-h-96 overflow-y-auto cursor-pointer`}
+      id="dropdown"
+      className={`${styles.dropdown} rounded absolute shadow-md inline-block m-2 max-w-xs min-w-md max-h-96 overflow-y-auto cursor-pointer`}
     >
       {items.map((item, index) => (
-        <li className="px-4 py-2 hover:bg-lightGray-300 last:mb-0" key={`${item.name}${index}`}>
+        <li
+          className={classNames({
+            'bg-white px-4 py-2 hover:bg-lightGray-300 last:mb-0': true,
+            'bg-lightGray-300': index === activeItemIndex,
+          })}
+          key={`${item.name}${index}`}
+          onMouseEnter={() => handleOnMouseEnter(index)}
+          onClick={() => handleItemClick(item)}
+        >
           <p>{item.name}</p>
           <p className="text-xs overflow-ellipsis text-midGray-500 mt-1">{item.description}</p>
         </li>
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import styles from './styles.module.css';
 import './styles/base.css';
-import { Dropdown } from './components/Dropdown';
+import { Dropdown, DropdownItem } from './components/Dropdown';
+import { EditableBlock } from './components/EditableBlock';
+import uuid from './utils/uuid';
 export interface MarkdownProps {
   placeholder?: string;
   triggerKey?: string;
@@ -11,43 +13,165 @@ const items = [
   {
     name: 'Text',
     description: 'Plain text',
+    tag: 'p',
   },
   {
     name: 'Heading 1',
     description: 'Big section heading',
+    tag: 'h1',
   },
   {
     name: 'Heading 2',
     description: 'Medium section heading',
+    tag: 'h2',
   },
   {
     name: 'Heading 3',
     description: 'Small section heading',
+    tag: 'h3',
   },
   {
-    name: 'Bullet list',
-    description: 'List with bullet points',
+    name: 'Heading 3',
+    description: 'Small section heading',
+    tag: 'h3',
   },
   {
-    name: 'Numbered',
-    description: 'List with numbering',
+    name: 'Link',
+    description: 'Convert to Link',
+    tag: 'h3',
+  },
+  {
+    name: 'Image',
+    description: 'Add Image',
+    tag: 'h3',
+  },
+  {
+    name: 'Blockquote',
+    description: 'Add Blockquote',
+    tag: 'h3',
+  },
+  {
+    name: 'List',
+    description: 'Add List',
+    tag: 'h3',
+  },
+  {
+    name: 'Horizontal Rule',
+    description: 'Add Horizontal Rule',
+    tag: 'h3',
+  },
+  {
+    name: 'Inline Code',
+    description: 'Add Inline Code',
+    tag: 'h3',
+  },
+  {
+    name: 'Code Block',
+    description: 'Add Code Block',
+    tag: 'h3',
   },
 ];
 
+const initalBlock = {
+  uuid: uuid(),
+  tag: 'p',
+  content: '',
+};
+
 export const Markdown = ({ placeholder = 'Type Something', triggerKey = '/' }: MarkdownProps) => {
+  const [activeItemIndex, setActiveItemIndex] = React.useState(0);
+  const [selectedItem, setSelectedItem] = React.useState<DropdownItem>();
   const [dropdownisOpen, setDropdownIsOpen] = React.useState(false);
-  const handleChange = (event: React.ChangeEvent<HTMLDivElement>) => {
-    if (event.target.innerText === triggerKey) {
-      setDropdownIsOpen(true);
-    } else if (dropdownisOpen) {
-      setDropdownIsOpen(false);
+  const [rangeValues, setRangeValues] = React.useState({ top: 0, left: 0 });
+  const [blocks, setBlocks] = React.useState([initalBlock]);
+  const [blockFocus, setBlockFocus] = React.useState(initalBlock.uuid);
+
+  const node = React.useRef<HTMLDivElement | null>(null);
+
+  React.useEffect(() => {
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
+  const handleClickOutside = (e: any) => {
+    if (node?.current?.contains(e?.target)) {
+      return;
     }
+
+    setDropdownIsOpen(false);
+  };
+
+  React.useEffect(() => {
+    console.log('blockFocus', blockFocus);
+    const element = document.getElementById(blockFocus);
+    element?.focus();
+  }, [blockFocus]);
+
+  React.useEffect(() => {
+    if (dropdownisOpen) {
+      let top = 0;
+      let left = 0;
+      let dropdown = document.getElementById('dropdown');
+      if (dropdown) {
+        top = rangeValues.top;
+        left = rangeValues.left;
+        if (rangeValues.left + dropdown.offsetWidth > window.innerWidth) {
+          left = left - (rangeValues.left + dropdown.offsetWidth - window.innerWidth) - 50;
+        }
+        if (rangeValues.top + dropdown.offsetHeight > window.innerHeight) {
+          top = top - (rangeValues.left + dropdown.offsetHeight - window.innerHeight) - 50;
+        }
+      }
+      dropdown?.setAttribute('style', `top:${top}px;left:${left}px`);
+    }
+  }, [dropdownisOpen]);
+
+  const handleOnMouseEnter = (index: number) => {
+    setActiveItemIndex(index);
+  };
+
+  const handleItemClick = (item: DropdownItem) => {
+    setSelectedItem(item);
+    setDropdownIsOpen(false);
+    alert('You selected ' + items[activeItemIndex].name);
   };
 
   return (
-    <React.Fragment>
-      <div contentEditable={true} className={`${styles.test}`} placeholder={placeholder} onInput={handleChange} />
-      <Dropdown isOpen={dropdownisOpen} items={items} />
-    </React.Fragment>
+    <div id="content" className={styles.markdown}>
+      {blocks?.map((block) => {
+        return (
+          <EditableBlock
+            key={block.uuid}
+            activeItemIndex={activeItemIndex}
+            dropdownisOpen={dropdownisOpen}
+            items={items}
+            placeholder={placeholder}
+            setActiveItemIndex={setActiveItemIndex}
+            setDropdownIsOpen={setDropdownIsOpen}
+            setRangeValues={setRangeValues}
+            setSelectedItem={setSelectedItem}
+            triggerKey={triggerKey}
+            block={block}
+            blocks={blocks}
+            setBlocks={setBlocks}
+            setBlockFocus={setBlockFocus}
+          />
+        );
+      })}
+
+      {dropdownisOpen && (
+        <div ref={node}>
+          <Dropdown
+            items={items}
+            activeItemIndex={activeItemIndex}
+            handleItemClick={handleItemClick}
+            handleOnMouseEnter={handleOnMouseEnter}
+          />
+        </div>
+      )}
+    </div>
   );
 };
